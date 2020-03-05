f33a7948bc358f27d38c0d1bf2a93e6806d9cef0
Nopharut CHADEE <nchadee@etud.u-pem.fr>
2020-03-05 02:24:27 +0100

    Implemented a better default camera

--- apps/gltf-viewer/ViewerApplication.cpp
+++ apps/gltf-viewer/ViewerApplication.cpp
@@ -139,29 +139,34 @@ int ViewerApplication::run()
   const auto normalMatrixLocation =
       glGetUniformLocation(glslProgram.glId(), "uNormalMatrix");
 
+  tinygltf::Model model;
+  if(!loadGltfFile(model)) {
+    return -1;
+  }
+  glm::vec3 bboxMin, bboxMax;
+  computeSceneBounds(model, bboxMin, bboxMax);
+
+  glm::vec3 
+      diagonal = bboxMax - bboxMin, 
+      center = (bboxMax + bboxMin) * 0.5f,
+      up = glm::vec3(0, 1, 0),
+      eye = diagonal.z > 0 ? center + diagonal : center + 2.f * glm::cross(diagonal, up);
+
   // Build projection matrix
-  auto maxDistance = 500.f; // TODO use scene bounds instead to compute this
+  auto maxDistance = glm::length(diagonal);
   maxDistance = maxDistance > 0.f ? maxDistance : 100.f;
   const auto projMatrix =
       glm::perspective(70.f, float(m_nWindowWidth) / m_nWindowHeight,
           0.001f * maxDistance, 1.5f * maxDistance);
 
-  // TODO Implement a new CameraController model and use it instead. Propose the
-  // choice from the GUI
   FirstPersonCameraController cameraController{
       m_GLFWHandle.window(), 0.5f * maxDistance};
   if (m_hasUserCamera) {
     cameraController.setCamera(m_userCamera);
   } else {
-    // TODO Use scene bounds to compute a better default camera
-    cameraController.setCamera(
-        Camera{glm::vec3(0, 0, 0), glm::vec3(0, 0, -1), glm::vec3(0, 1, 0)});
+    cameraController.setCamera(Camera{eye, center, up});
   }
 
-  tinygltf::Model model;
-  if(!loadGltfFile(model)) {
-    return -1;
-  }
   const std::vector<GLuint> bufferObjects = createBufferObjects(model);
   std::vector<VaoRange> meshToVertexArrays;
   const std::vector<GLuint> vertexArrayObjects = createVertexArrayObjects(model, bufferObjects, meshToVertexArrays);
